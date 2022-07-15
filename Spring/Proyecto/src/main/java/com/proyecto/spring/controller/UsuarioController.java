c1961f1a49a19dee17533f0a45cf81ca23644a49
Brandon Avendaño Villegas <42923366+LanIkari@users.noreply.github.com>
2022-07-14 22:09:07 -0500

    Add files via upload

---
+++ Spring/Proyecto/src/main/java/com/proyecto/spring/controller/UsuarioController.java
@@ -0,0 +1,86 @@
+package com.proyecto.spring.controller;
+
+
+import com.azure.cosmos.models.PartitionKey;
+import com.proyecto.spring.repository.UsuarioDBRepository;
+import com.proyecto.spring.dto.UsuarioCrudResponse;
+import com.proyecto.spring.model.Usuario;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    UsuarioDBRepository usuarioDBRepository;
+
+
+    Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
+    //Agregar nuevo usuario
+    @PostMapping
+    public ResponseEntity<UsuarioCrudResponse> createNewUsuario(@RequestBody Usuario u) {
+
+        u = usuarioDBRepository.save(u);
+        UsuarioCrudResponse usuarioCrudResponse = new UsuarioCrudResponse();
+        usuarioCrudResponse.setMessage("Nuevo usuario creado correctamente con el ID: " + u.getId());
+        usuarioCrudResponse.setStatusCode("Ok");
+        return new ResponseEntity<UsuarioCrudResponse>(usuarioCrudResponse, HttpStatus.CREATED);
+    }
+
+    //Actualizar un usuario existente
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateExistingUsuario(@PathVariable String id, @RequestBody Usuario u) {
+
+        usuarioDBRepository.save(u);
+        return new ResponseEntity<String>("", HttpStatus.NO_CONTENT);
+    }
+
+
+    //get customer details
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Usuario>> getUsuarios(@PathVariable String id) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("ContentType", "application/json");
+        List<Usuario> usuarioList = new ArrayList<>();
+
+        logger.info("Id is present in the GET request");
+        List<Optional<Usuario>> optionaUsuarioList = Collections.singletonList(usuarioDBRepository.findById(id));
+        if (!(optionaUsuarioList.get(0).isEmpty())) {
+            optionaUsuarioList.stream().forEach(u -> u.ifPresent(usuario -> usuarioList.add(usuario)));
+            return new ResponseEntity<List<Usuario>>(usuarioList, responseHeaders, HttpStatus.OK);
+        }
+
+
+        return new ResponseEntity<List<Usuario>>(usuarioList, responseHeaders, HttpStatus.NOT_FOUND);
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> getAllUsuarios() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("ContentType", "application/json");
+        List<Usuario> usuarioList = new ArrayList<>();
+
+        logger.info("Id is not present in the GET request");
+        usuarioList = usuarioDBRepository.getAllUsuarios();
+        return new ResponseEntity<List<Usuario>>(usuarioList, responseHeaders, HttpStatus.OK);
+
+    }
+
+    //delete the customer of a particular id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteExistingUsuario(@PathVariable String id) {
+        Optional<Usuario> usuario = usuarioDBRepository.findById(id);
+        usuarioDBRepository.deleteById(id, new PartitionKey(usuario.get().getNombre_usuario()));
+        return new ResponseEntity<String>("", HttpStatus.NO_CONTENT);
+    }
+}
---
+++ Spring/Proyecto/src/main/java/com/proyecto/spring/dto/UsuarioCrudResponse.java
@@ -0,0 +1,10 @@
+package com.proyecto.spring.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsuarioCrudResponse {
+    private String statusCode;
+    private String message;
+}
